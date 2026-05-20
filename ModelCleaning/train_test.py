1bc4d2369ee3cf93c0dd0b8cb00461a0000a2c33
YuliiaRysinska <157659108+YuliiaRysinska@users.noreply.github.com>
2026-05-20 16:17:46 +0000

    update train test file

--- ModelCleaning/train_test.py
+++ ModelCleaning/train_test.py
@@ -1,31 +1,32 @@
 import os
-import pickle
 import mlflow
-
 from pandas import read_csv
+from joblib import dump
+
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
 
-# MLflow tracking
-mlflow.set_tracking_uri("http://localhost:5555")
+mlflow.set_tracking_url("http://localhost:5555")
 
-# Workspace path
-workspace = os.getenv("GITHUB_WORKSPACE", os.getcwd())
+# Get GitHub workspace path
+workspace = os.getenv('GITHUB_WORKSPACE')
 
-# Dataset path
-model_cleaning_dir = os.path.join(workspace, "ModelCleaning")
-csv_file_path = os.path.join(model_cleaning_dir, "cleaned_data.csv")
+# Define ModelCleaning directory
+model_cleaning_dir = os.path.join(workspace, 'ModelCleaning')
 
-# Check file
+# Define path to cleaned CSV file
+csv_file_path = os.path.join(model_cleaning_dir, 'cleaned_data.csv')
+
+# Check if file exists
 if os.path.exists(csv_file_path):
     print(f"File found: {csv_file_path}")
 else:
     print(f"File not found at: {csv_file_path}")
 
-# Load dataset
+# Read cleaned dataset
 df = read_csv(csv_file_path)
 
+# Show first rows
 print(df.head())
 
 # Features and target
@@ -39,41 +40,32 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     random_state=42
 )
-
-# Start MLflow run
+# Start MLflow Run
 with mlflow.start_run():
-
     # Train model
     mind = LinearRegression()
     mind.fit(X_train, y_train)
 
-    # Predict
-    predictions = mind.predict(X_test)
-
-    # Metrics
+    # Evaluate
+    predictions = model.predict(X_test)
     r2 = r2_score(y_test, predictions)
     mse = mean_squared_error(y_test, predictions)
 
-    # Log metrics
+    # Log Metrics
     mlflow.log_metric("r2_score", r2)
     mlflow.log_metric("mse", mse)
 
-    # Log model
-    result = mlflow.sklearn.log_model(
-        sk_model=mind,
-        artifact_path="model"
-    )
+    # Log Model & Resiter
+    result = mlflow.sklearn.log_model(sk_model=mind, artifact_path="model")
 
-    # Register model
     mlflow.register_model(
-        model_uri=result.model_uri,
-        name="food_model"
-    )
+        model_url=result.model_url,
+        name="my_linear_model"
+        )
+    print(f"Model logged by R2: {r2}")
 
-    print(f"Model logged with R2: {r2}")
-
-# Save model locally
-with open("Food_Model.pkl", "wb") as file:
+with open("Food Model.pkl", "wb") as file:
     pickle.dump(mind, file)
+
 
 print("Model trained and saved successfully!")
