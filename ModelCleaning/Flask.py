dcdecc24d9c0567bb71ae7bd7d12d0d082d20c0e
YuliiaRysinska <157659108+YuliiaRysinska@users.noreply.github.com>
2026-05-21 09:29:46 +0000

    update Flask file

---
+++ ModelCleaning/Flask.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load('models/model.pkl')
+
+@app.route('/health')
+def health():
+    return jsonify({'status': 'healthy'})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['features']
+
+    prediction = model.predict([data])
+
+    return jsonify({
+        'prediction': int(prediction[0])
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
